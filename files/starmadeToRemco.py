9950f52db28c9e6e4d2e4ce34f3f3830f0eb0100
Nathan Snow <nathan.snow@mimir-tech.org>
2019-07-19 11:24:19 -0600

    updating starmadeToRemco.py

--- files/starmadeToRemco.py
+++ files/starmadeToRemco.py
@@ -4,15 +4,15 @@ import re
 regex = re.compile('(\w+)\s=\s(.*?)\s(\/\/.*)')
 data = ''
 
-with open('server.cfg', 'r') as fh:
-  lines = fh.readlines()
-  for line in lines:
-    try:
-      match = regex.search(line)
-      data += (match.group(1) + " = {{ getenv(\"" + match.group(1) + "\", \"" + match.group(2) + "\") }} " + match.group(3) + "\n")
-    except:
-      pass
+try:
+  with open('server.cfg', 'r') as fh:
+    lines = fh.readlines()
+    for line in lines:
+        match = regex.search(line)
+        data += (match.group(1) + " = {{ getenv(\"" + match.group(1) + "\", \"" + match.group(2) + "\") }} " + match.group(3) + "\n")
 
-with open('server.cfg.new', 'w') as fh:
-  fh.write(data)
+  with open('server.cfg.new', 'w') as fh:
+    fh.write(data)
+except Exception as err:
+  print(str(err))
 
