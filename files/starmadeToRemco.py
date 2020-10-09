f7e95ce9e5549f22cc13d7e85fba1919dbd772b4
Nathan Snow <nathan.snow@mimir-tech.org>
2020-10-09 04:52:31 +0000

    updating template generator script

--- files/starmadeToRemco.py
+++ files/starmadeToRemco.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import re
 
-regex = re.compile('(\w+)\s=\s(.*?)\s(\/\/.*)')
+regex = re.compile('(\w+)\s?=\s?(.*?)?\s?(\/\/.*)')
 data = ''
 
 try:
@@ -9,11 +9,14 @@ try:
     lines = fh.readlines()
     for line in lines:
         match = regex.search(line)
-        data += (match.group(1) + " = {{ getv(\"/starmade/" + match.group(1).replace("_", "-").lower() + "\", \"" + match.group(2) + "\") }} " + match.group(3) + "\n")
+        if len(match.groups()) < 3 and '//' in match.group(2):
+            data += (match.group(1) + " = {{ getv(\"/starmade/" + match.group(1).replace("_", "-").lower() + "\", \"\") }} " + match.group(2) + "\n")
+        else:
+            data += (match.group(1) + " = {{ getv(\"/starmade/" + match.group(1).replace("_", "-").lower() + "\", \"" + match.group(2) + "\") }} " + match.group(3) + "\n")
         print(data)
 
   with open('server.cfg.new', 'w') as fh:
     fh.write(data)
 except Exception as err:
-  print(str(err))
+    raise
 
